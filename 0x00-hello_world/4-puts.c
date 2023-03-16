ab78afa48d7c0017797284e139f80df07e6a0cb3
Mostafa-Abdelfath <mostafaelweshy777@gmail.com>
2023-03-16 15:00:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
+/**
+ * Description: main fuction
+ * return 0 if no errors 
+ * print a sentence using puts
+ **/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
