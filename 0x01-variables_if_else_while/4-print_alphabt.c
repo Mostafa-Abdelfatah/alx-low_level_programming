730aa9ad98400de448c4678bc95097bf4ab11dc8
Mostafa-Abdelfath <mostafaelweshy777@gmail.com>
2023-03-19 18:29:09 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphapets
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
