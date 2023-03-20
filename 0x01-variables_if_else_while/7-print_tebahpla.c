37d1f37dc76084b8749eafd9ec5037764ad9657a
Mostafa-Abdelfath <mostafaelweshy777@gmail.com>
2023-03-19 18:58:04 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ * in reverse
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}	
