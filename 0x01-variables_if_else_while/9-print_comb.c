85ef2e3c59e6c31b8034e5b87873b3c2a2fab1a8
Mostafa-Abdelfath <mostafaelweshy777@gmail.com>
2023-03-19 19:19:10 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * from 0 to 9
+ * separated by " , "
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
