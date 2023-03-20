e7a2771dc2c971d56b0a867273e20b76e3ff1c74
Mostafa-Abdelfath <mostafaelweshy777@gmail.com>
2023-03-19 18:43:04 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * from 0 to 9
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char a;
+
+	for(a = '0'; a <= '9'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
