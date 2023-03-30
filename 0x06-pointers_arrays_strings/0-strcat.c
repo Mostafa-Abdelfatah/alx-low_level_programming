e80568f04e95ed5ce83f48b8199d938b221cc788
Mostafa-Abdelfath <mostafaelweshy777@gmail.com>
2023-03-29 23:14:05 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - to concatinate a two strings
+ * @dest : carry the second string
+ * @src : carry the source string
+ *
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int d, s;
+
+    d = 0;
+    s = 0;
+
+    while (*(dest + d) != '\0')
+    {
+        d++;
+    }
+    while (*(src + s) != '\0')
+    {
+        *(dest + d) = *(src + s);
+        d++;
+        s++;
+    }
+    return (dest);
+}
