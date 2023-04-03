e9ab0ce8b78c2fe79f12d01ae382fd353a69a8de
mostafa abdlfatah <mostafaelweshy777@gmail.com>
2023-04-03 17:28:21 +0200

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* _strncpy - copy a string
+* @dest: carry destination string
+* @src: carry source string
+* Return: Always 0
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int s, c;
+
+s = 0;
+
+while (*(src + s) != '\0')
+{
+s++;
+}
+
+if (s <= n)
+{
+dest = src;
+}
+else
+{
+for (c = 0; c < n; c++)
+{
+    *(dest + c) = *(src + c);
+}
+}
+
+return (dest);
+}
