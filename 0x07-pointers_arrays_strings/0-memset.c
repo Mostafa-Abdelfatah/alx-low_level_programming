56ee9f542679f513ad0d71cf377ad31105851600
mostafa abdlfatah <mostafaelweshy777@gmail.com>
2023-04-04 03:17:02 +0200

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,32 +1,19 @@
 #include "main.h"
-
 /**
- * _strncat - to concatinate a two strings
- * @dest: carry the second string
- * @src: carry the source string
- * Return: string
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
  */
-
-char *_strcat(char *dest, char *src)
+char *_memset(char *s, char b, unsigned int n)
 {
-    int LenD, LenS;
+	unsigned int i = 0;
 
-    LenD = 0;
-    LenS = 0;
-
-    while (*(dest + LenD) != '\0')
-    {
-        LenD++;
-    }
-
-    while (*(src + LenS) != '\0' && LenD < 97)
-    {
-        *(dest + LenD) = *(src + LenS);
-        LenD++;
-        LenS++;
-    }
-
-    *(dest + LenD) = '\0';
-
-    return dest;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
