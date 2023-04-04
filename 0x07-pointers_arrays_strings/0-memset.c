af9fa7c22327c0fb6f8e65b61b3020a2f01975af
mostafa abdlfatah <mostafaelweshy777@gmail.com>
2023-04-04 03:15:04 +0200

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - to concatinate a two strings
+ * @dest: carry the second string
+ * @src: carry the source string
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int LenD, LenS;
+
+    LenD = 0;
+    LenS = 0;
+
+    while (*(dest + LenD) != '\0')
+    {
+        LenD++;
+    }
+
+    while (*(src + LenS) != '\0' && LenD < 97)
+    {
+        *(dest + LenD) = *(src + LenS);
+        LenD++;
+        LenS++;
+    }
+
+    *(dest + LenD) = '\0';
+
+    return dest;
+}
