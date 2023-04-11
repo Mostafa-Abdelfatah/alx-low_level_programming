f40109375868e3f97705c8897fe1e267e8c45019
mostafa abdlfatah <mostafaelweshy777@gmail.com>
2023-04-11 03:04:18 +0200

    0 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - print the program name
-* @argc: int
-* @argv: string array
-* Return: 0
-*/
-
-int main (int argc, char *argv[])
+ * main - Prints the program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char* argv[])
 {
-(void) argc;
-printf("%s\n", argv[0]);
-return (0);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - print the number of arguments
-* @argc: int
-* @argv: string array
-* Return: 0
-*/
-
-int main (int argc, char *argv[] __attribute__((unsed)))
+ * main - prints the number of arguements passed into the program
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-(void) argv;
-printf("%d\n", argc - 1);
-return (0);
+	(void)argv;
+	printf("%i\n", argc - 1);
+	return (0);
 }
