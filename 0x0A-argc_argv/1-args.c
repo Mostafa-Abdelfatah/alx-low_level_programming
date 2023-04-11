287d86f9c6ccf7f2021c6f37dfd22cd15d0637f7
mostafa abdlfatah <mostafaelweshy777@gmail.com>
2023-04-11 02:47:42 +0200

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the number of argumments
+* @argc: int
+* @argv: string array
+* Return: 0
+*/
+
+int main (int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
