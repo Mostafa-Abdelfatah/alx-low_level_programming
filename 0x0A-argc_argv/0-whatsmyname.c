8966a57262eccf5d89101ae64b66a51b3dd03782
mostafa abdlfatah <mostafaelweshy777@gmail.com>
2023-04-11 02:45:17 +0200

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the program name
+* @argc: int
+* @argv: string array
+* Return: 0
+*/
+
+int main (int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
