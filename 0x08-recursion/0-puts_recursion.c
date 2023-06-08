1bf7f39af42a05a0cc4022055a98e9075579b220
mostafa abdelfatah <mostafaelweshy777@gmail.com>
2023-06-08 20:21:39 +0200

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * _puts_recursion - prints a string
  * @s: pointer to string to print
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
@@ -13,7 +11,5 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-	
 		_putchar('\n');
-
 }
